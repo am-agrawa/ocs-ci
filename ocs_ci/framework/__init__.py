efbbafdb4c9eeb728a1c7f12e157811d9e6137d7
Shylesh Kumar Mohan <shmohan@redhat.com>
2021-04-12 15:15:01 -0700

    Flexible component deployment
    Make components like rgw, cephfs, noobaa, cephblockpool
    to be flexibly deplyed.
    
    Signed-off-by: Shylesh Kumar Mohan <shmohan@redhat.com>

--- ocs_ci/framework/__init__.py
+++ ocs_ci/framework/__init__.py
@@ -28,6 +28,7 @@ class Config:
     UPGRADE: dict = field(default_factory=dict)
     FLEXY: dict = field(default_factory=dict)
     UI_SELENIUM: dict = field(default_factory=dict)
+    COMPONENTS: dict = field(default_factory=dict)
 
     def __post_init__(self):
         self.reset()
